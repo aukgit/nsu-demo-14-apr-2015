b94ad0e8a6b76222cf179d191b274ae9a6990e20
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-12 02:53:37 +0600

    basic controller sample

---
+++ finished_stage/NSUDemo/Controllers/BasicController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSUDemo.Controllers
+{
+    public class BasicController : Controller
+    {
+        // GET: Basic
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Alim() {
+            return "Hello! Alim Ul Karim";
+        }
+        public ViewResult GiveView() {
+            return View();
+        }
+    }
+}
--- finished_stage/NSUDemo/Controllers/EventController.cs
+++ finished_stage/NSUDemo/Controllers/EventController.cs
@@ -3,87 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NSUDemo.Models;
 
-namespace NSUDemo.Controllers
-{
-    public class EventController : Controller
-    {
+namespace NSUDemo.Controllers {
+    public class EventController : Controller {
+
+        private EntityDatabase db = new EntityDatabase();
         void GetDropDown() {
-            
+            ViewBag.EventCategoryId = new SelectList(db.EventCategories.ToList(), "EventCategoryId", "EventCategoryName");
         }
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index() {
+            var events = db.Events.ToList();
+            return View(events);
         }
 
         // GET: Event/Details/5
-        public ActionResult Details(int id)
-        {
+        public ActionResult Details(int id) {
             return View();
         }
 
         // GET: Event/Create
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
+            GetDropDown();
             return View();
         }
 
         // POST: Event/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
+        public ActionResult Create(Event @event) {
+            try {
+                if (ModelState.IsValid) {
+                    db.Events.Add(@event);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View();
+            } catch {
                 return View();
             }
         }
 
         // GET: Event/Edit/5
-        public ActionResult Edit(int id)
-        {
+        public ActionResult Edit(int id) {
             return View();
         }
 
         // POST: Event/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
+        public ActionResult Edit(int id, FormCollection collection) {
+            try {
                 // TODO: Add update logic here
 
                 return RedirectToAction("Index");
-            }
-            catch
-            {
+            } catch {
                 return View();
             }
         }
 
         // GET: Event/Delete/5
-        public ActionResult Delete(int id)
-        {
+        public ActionResult Delete(int id) {
             return View();
         }
 
         // POST: Event/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
+        public ActionResult Delete(int id, FormCollection collection) {
+            try {
                 // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
-            }
-            catch
-            {
+            } catch {
                 return View();
             }
         }
