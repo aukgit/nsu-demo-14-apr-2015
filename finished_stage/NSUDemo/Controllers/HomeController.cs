3bea0fb03e9c5aa80ea3e5babf8053aa80538c90
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-12 04:51:27 +0600

    complete

--- finished_stage/NSUDemo/Controllers/HomeController.cs
+++ finished_stage/NSUDemo/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NSUDemo.Models;
 
 namespace NSUDemo.Controllers {
     public class HomeController : Controller {
+
+        EntityDatabase db = new EntityDatabase();
         public ActionResult Index() {
-            return View();
+            var events = db.Events.OrderByDescending(n=> n.EventID).Take(3);
+            return View(events);
         }
 
         public ActionResult About() {
