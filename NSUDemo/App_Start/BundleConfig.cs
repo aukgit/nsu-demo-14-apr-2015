ea9a71f35756df1c4b2ab22d657d9991260b0004
Alim Ul Karim <devorg.bd@gmail.com>
2015-04-12 01:42:37 +0600

    themes added

--- NSUDemo/App_Start/BundleConfig.cs
+++ NSUDemo/App_Start/BundleConfig.cs
@@ -18,15 +18,30 @@ namespace NSUDemo {
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/wow.min.js",
+                      "~/Scripts/toastr.min.js",
+                      "~/Scripts/owl.carousel.min.js",
+                      "~/Scripts/moment.min.js",
+                      "~/Scripts/greenshock/TweenMax.js",
+                      "~/Scripts/Custom.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/css/site.css",
+                        "~/Content/css/less-imports.css",
+                        "~/Content/css/animate.min.css",
+                        "~/Content/css/font-awesome.min.css",
+                        "~/Content/css/color-fonts.css",
+                        "~/Content/css/override-mvc.css",
+                        "~/Content/css/front-developer.css",
+                        "~/Content/css/utilities.css"
+                                        ));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
